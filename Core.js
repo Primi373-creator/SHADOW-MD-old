6daf23e315aa4d82e8b1aee2715fcb365ebfd266
Cipher0071 <156423967+Cipher0071@users.noreply.github.com>
2024-01-18 11:43:49 +0100

    SHADOW-MD

--- Core.js
+++ Core.js
@@ -43,7 +43,13 @@ const os = require('os');       // for os info
 const gis = require("g-i-s");
 const { MessageType } = require('@whiskeysockets/baileys');
 //"parse-ms": "^1.1.0",
-
+//================================================================================================================//
+function getRandomFile(path) {
+  const files = fs.readdirSync(path);
+  const randomIndex = Math.floor(Math.random() * files.length);
+  return files[randomIndex];
+}
+//================================================================================================================//
 
 //
 let nowtime = '';
@@ -6946,14 +6952,11 @@ _Click the button below to download_`
   ╰━━━━━━━━━━━━━━⊷ `
    
    let buttonMessage = {
-          image: fs.readFileSync('./system/sha10.jpg'),
-          caption: helpmenu,
-
-          headerType: 4
-
-        }
-        shadow.sendMessage(m.chat, buttonMessage, { quoted: m })
-      }
+  image: fs.readFileSync(`./system/${getRandomFile('./system/')}`),
+  caption: helpmenu,
+  headerType: 4
+};
+     shadow.sendMessage(m.chat, buttonMessage, { quoted: m });
         break;
        
 //=========================================================================================//
